739bdd034baf498fcacf4442b5edbde105cff557
EthanBurnsides <ethanyazelart@gmail.com>
2014-01-26 13:12:33 -0500

    Added First Cut Scenes, Title Scene, and audio.
    Read Title.

---
+++ Assets/Scripts/TitleScreenButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		Debug.Log("Felt mouse down.");
+		Application.LoadLevel ("StoryBoard_scene1");
+	}
+
+	void OnMouseUp(){
+
+	}
+}
