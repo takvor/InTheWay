58dc5ed6d5f7a19e782598bdfd6060ead6bbc56e
dragifon <dragifon@aim.com>
2014-01-26 15:10:46 -0500

    Updated GUIScript and messed with Opossum prefabs

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class GUIScript : MonoBehaviour {
-	private bool possumDrag = false;
+	private bool opossumDrag = false;
 	private enum SpritesToPlace{
-		Fred,
-		OtherRealNames,
+		Normal_Possum,
+		Spike_Possum,
+		Bomb_Possum,
 		None
 	
 	};
-	private SpritesToPlace nextSpriteToPlace = SpritesToPlace.None;
+	//private SpritesToPlace nextSpriteToPlace = SpritesToPlace.None;
 	// Use this for initialization
 	GameObject clone;
 	Vector2 currentMouse;
@@ -22,28 +23,24 @@ public class GUIScript : MonoBehaviour {
 
 		currentMouse  = new Vector3(Input.mousePosition.x,Input.mousePosition.y);
 		Vector3 currentPos = Camera.main.ScreenToWorldPoint(currentMouse);
-		Vector3 newPos = new Vector3(currentPos.x, currentPos.y, 90);
+		Vector3 newPos = new Vector3(currentPos.x, currentPos.y,0);
 
 
 		//transform.position = currentPos;
 
-		if(possumDrag){
+		if(opossumDrag){
 			clone.transform.position = newPos;
-			Debug.Log ("Drag The Possum! " + newPos.x.ToString() + ", " + newPos.y.ToString());
-			/*cols = GetComponents<Collider2D>();
-			foreach(Collider2D c in cols){
-				c.isTrigger = true; //allows possum to ignore collisions
-			}*/
+			Debug.Log ("Drag The Opossum! " + newPos.x.ToString() + ", " + newPos.y.ToString());
 			
 			if(Input.GetMouseButtonDown(0)){
 				Collider2D[] cols = clone.GetComponents<Collider2D>();
 				foreach(Collider2D c in cols){
-					c.isTrigger = false; //resets possum to have collisions
+					c.isTrigger = false; //resets opossum to have collisions
 				}
 				//Vector3  currentScreenPoint = new Vector3(Input.mousePosition.x,Input.mousePosition.y,90);
 
-				possumDrag = false;
-				nextSpriteToPlace = SpritesToPlace.None;
+				opossumDrag = false;
+				//nextSpriteToPlace = SpritesToPlace.None;
 			}
 		}
 	}
@@ -51,34 +48,49 @@ public class GUIScript : MonoBehaviour {
 	void OnGUI () {
 
 
-		// Make a background box
-		GUI.Box(new Rect(Screen.width/16,Screen.height-(Screen.height/8),Screen.width-(Screen.width/8),Screen.height/8), "Menu Box");
-
-
 
 		// Make a first button
-		if(GUI.Button (new Rect(Screen.width/16,0f,Screen.width/16,Screen.height/8), "Spawn Possum")){ //Screen.height-(Screen.height/8)
-			nextSpriteToPlace = SpritesToPlace.Fred;
-			setClone();
+		if(GUI.Button (new Rect(Screen.width/8,Screen.height-(Screen.height/8),Screen.width/4,Screen.height/8), "Normal Opossum")){
+			//nextSpriteToPlace = SpritesToPlace.Normal_Possum;
+			//setClone();
+			clone = Instantiate (Resources.Load("Characters/Normal_Opossum"))as GameObject;
 			Collider2D[] cols = clone.GetComponents<Collider2D>();
 			foreach(Collider2D c in cols){
-				c.isTrigger = true; //allows possum to ignore collisions
+				c.isTrigger = true; //allows opossum to ignore collisions
 			}
-			possumDrag = true;
+			opossumDrag = true;
 
 		}
 
 		// Make a second button
-		if(GUI.Button (new Rect((Screen.width/16)*2,Screen.height-(Screen.height/8),Screen.width/16,Screen.height/8), "Button 2")){
-			Debug.Log("You clicked me"); //Button function goes here
+		if(GUI.Button (new Rect((Screen.width/8)*3,Screen.height-(Screen.height/8),Screen.width/4,Screen.height/8), "Spike Opossum")){
+			//nextSpriteToPlace = SpritesToPlace.Spike_Possum;
+			//setClone();
+			clone = Instantiate (Resources.Load("Characters/Spike_Opossum"))as GameObject;
+			Collider2D[] cols = clone.GetComponents<Collider2D>();
+			foreach(Collider2D c in cols){
+				c.isTrigger = true; //allows opossum to ignore collisions
+			}
+			opossumDrag = true;
+		}
+
+		// Make a third button
+		if(GUI.Button (new Rect((Screen.width/8)*5,Screen.height-(Screen.height/8),Screen.width/4,Screen.height/8), "Bomb Opossum")){
+			//nextSpriteToPlace = SpritesToPlace.Bomb_Possum;
+			//setClone();
+			clone = Instantiate (Resources.Load("Characters/Bomb_Opossum"))as GameObject;
+			Collider2D[] cols = clone.GetComponents<Collider2D>();
+			foreach(Collider2D c in cols){
+				c.isTrigger = true; //allows opossum to ignore collisions
+			}
+			opossumDrag = true;
 		}
 	}
 
-	private void setClone(){
+	/*private void setClone(){
 		if(nextSpriteToPlace != SpritesToPlace.None){
-			Debug.Log ("Possum was made.");
+			Debug.Log ("Opossum was made.");
 			clone = Instantiate (Resources.Load("Characters/" + nextSpriteToPlace.ToString()))as GameObject;
 		}
-			//clone.name = "Possum 1";
-	}
+	}*/
 }
