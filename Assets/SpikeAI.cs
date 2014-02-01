de16a51fc66a77dce961ab721520378305185d95
dragifon <dragifon@aim.com>
2014-01-31 21:31:57 -0500

    Updated SpikeAI and collision boxes.
    Got SpikeAI script to make spike opossum move and edited collision boxes
    on opossums and duckling to be a little more accurate.

--- Assets/SpikeAI.cs
+++ Assets/SpikeAI.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class SpikeAI : MonoBehaviour {
+	
+	public float moveSpeed = 5f;
+	public float moveForce = 365f;
+	public float moveLeft = 0;
 
-	// Use this for initialization
-	void Start () {
-	
+	//private Animator anim;
+
+	void Awake () {
+		//anim = GetComponent<Animator>();
+		moveLeft -= 1;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		//if(rigidbody2D.velocity.x < maxSpeed)
+			// ... add a force to the player.
+		rigidbody2D.velocity = new Vector2(transform.localScale.x * moveLeft * moveSpeed,rigidbody2D.velocity.y);
+		Debug.Log ("Move Dang it!");
+		//rigidbody2D.AddForce(Vector2.right * moveForce);
+		//Debug.Log ("Moving Opossums");
 	}
 }
